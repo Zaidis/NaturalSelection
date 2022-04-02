33670dd97d2c1ae5f7d3448f6dcbaace3b49086e
Zaidis <chewybinx@gmail.com>
2022-04-01 21:16:18 -0500

    Genetic Algorithm

---
+++ NaturalSelectionEditor/Assets/Scripts/NaturalSelection/BunnyStats.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyStats : MonoBehaviour
+{
+    
+    public float fertality{get; set;} //mom
+    public float speed{get; set;}
+    public float intelligence{get; set;}
+    public float earSize{get; set;} //mom
+    public float growth{get; set;} 
+    public float foodConsumption{get; set;} //dad
+    public float pregnancyDuration{get; set;}
+    public int gender;
+    public float mutationRate{get; set;}
+
+    public float jumpHeight{get; set;} //dad
+}
