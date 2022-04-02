b6890470f0d204e4869727f970552235d11273fc
Dylan Oelke <dylanoelke.business@gmail.com>
2022-04-02 16:55:44 -0500

    Update bunny gore

--- NaturalSelectionEditor/Assets/BunnyKiller.cs
+++ NaturalSelectionEditor/Assets/BunnyKiller.cs
@@ -10,6 +10,10 @@ public class BunnyKiller : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Bunny"))
         {
+            GameObject c = Instantiate(decals[Random.Range(0, decals.Count)], other.transform.position, Quaternion.identity);
+            c.transform.LookAt(this.transform, Vector3.up);
+            c.transform.SetParent(this.transform);
+            c.GetComponent<Projector>().ignoreLayers = LayerMask.NameToLayer("Ground");
             //Hit a bunny. DIE
             GameObject t = Instantiate(decals[Random.Range(0, decals.Count)], other.transform.position + Vector3.up, Quaternion.identity);
             Instantiate(gibblet, other.transform.position + new Vector3(0,0.5f,0), other.transform.rotation);
