fd29d8a6c1f79ff10bdf37a5f25a823d7030a9d5
Zaidis <chewybinx@gmail.com>
2022-04-02 14:23:51 -0500

    Clouds done?

--- NaturalSelectionEditor/Assets/Scripts/Cloud.cs
+++ NaturalSelectionEditor/Assets/Scripts/Cloud.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-    
+    private float deathTimer = 60;
+    private void Update() { 
+        //cloud movement
+        transform.position += Vector3.forward * 0.05f;
 
+        deathTimer -= Time.deltaTime;
+
+        if(deathTimer <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
 
 }
