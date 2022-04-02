eae244c8fadf93555cdb0cc4a3652ad0ead8cfaa
Zaidis <chewybinx@gmail.com>
2022-04-02 12:26:47 -0500

    Carrot Prefab + Spawn Logic

--- NaturalSelectionEditor/Assets/Scripts/NaturalSelection/CarrotLogic.cs
+++ NaturalSelectionEditor/Assets/Scripts/NaturalSelection/CarrotLogic.cs
@@ -4,10 +4,48 @@ using UnityEngine;
 
 public class CarrotLogic : MonoBehaviour
 {
-
+    public float yOffset = 0.2f;
     public GameObject carrot;
-    private List<Vector3> wayPoints = new List<Vector3>();
+    public Terrain terrain;
 
+    [SerializeField] private float timer;
+    [SerializeField] private float maxTimer = 20;
+    private float terrainWidth;
+    private float terrainLength;
+    private float xTerrain;
+    private float zTerrain;
+    public void Start() {
+        timer = maxTimer;
+        //Get terrain size
+        terrainWidth = terrain.terrainData.size.x;
+        terrainLength = terrain.terrainData.size.z;
 
+        //Get terrain position
+        xTerrain = terrain.transform.position.x;
+        zTerrain = terrain.transform.position.z;
+
+        for (int i = 0; i < 100; i++) SpawnCarrot();
+
+    }
+
+    private void Update() {
+        timer -= Time.deltaTime;
+        if(timer <= 0) {
+            for (int i = 0; i < 100; i++) SpawnCarrot();
+            timer = maxTimer;
+        }
+    }
+    private void SpawnCarrot() {
+        //Generate random x,z,y position on the terrain
+        float x = Random.Range(xTerrain, xTerrain + terrainWidth);
+        float z = Random.Range(zTerrain, zTerrain + terrainLength);
+        float y = Terrain.activeTerrain.SampleHeight(new Vector3(x, 0, z));
+
+        //Apply Offset if needed
+        y += yOffset;
+
+        //Generate the Prefab on the generated position
+        GameObject objInstance = Instantiate(carrot, new Vector3(x, y, z), Quaternion.identity);
+    }
 
 }
