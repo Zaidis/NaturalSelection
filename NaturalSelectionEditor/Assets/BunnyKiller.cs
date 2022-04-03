d88cdb6a529935582076f04ed549262dd77e810d
Dylan Oelke <dylanoelke.business@gmail.com>
2022-04-02 20:09:04 -0500

    Trailer Spawning

--- NaturalSelectionEditor/Assets/BunnyKiller.cs
+++ NaturalSelectionEditor/Assets/BunnyKiller.cs
@@ -8,10 +8,14 @@ public class BunnyKiller : MonoBehaviour
     [SerializeField] GameObject gibblet;
     public static List<GameObject> DECALS = new List<GameObject>();
     public static GameObject GIBBLET;
+    public static int numOfKills;
+    [SerializeField] float decalLifeTime;
+    static float DECALLIFETIME;
     private void Awake()
     {
         DECALS = decals;
         GIBBLET = gibblet;
+        DECALLIFETIME = decalLifeTime;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -22,12 +26,20 @@ public class BunnyKiller : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Bunny"))
         {
+            numOfKills++;
+            TrailerSpawner ts = FindObjectOfType<TrailerSpawner>();
+            ts.AddCorpse();
+            if (numOfKills % TrailerSpawner.corpsesPerTrailer == 0)
+            {
+                ts.spawnTrailer = true;
+            }
             if (truckDecal)
             {
                 GameObject c = Instantiate(DECALS[Random.Range(0, DECALS.Count)], other.transform.position, Quaternion.identity);
                 c.transform.LookAt(truck, Vector3.up);
                 c.transform.SetParent(truck);
                 c.GetComponent<DecalProjector>().decalLayerMask = DecalLayerEnum.DecalLayer1;
+                Destroy(c, DECALLIFETIME);
             }
             
             //Hit a bunny. DIE
@@ -36,6 +48,7 @@ public class BunnyKiller : MonoBehaviour
             t.GetComponent<DecalProjector>().decalLayerMask = DecalLayerEnum.DecalLayer2;
             Instantiate(GIBBLET, other.transform.position + new Vector3(0, 0.5f, 0), other.transform.rotation);
             t.transform.eulerAngles = new Vector3(90, truck.rotation.eulerAngles.y, 0);
+            Destroy(t, DECALLIFETIME);
             BunnyAI bAI = other.GetComponent<BunnyAI>();
             if (bAI != null)
             {
