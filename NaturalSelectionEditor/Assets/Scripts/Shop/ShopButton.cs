5553a62a01cb5c6c97428b44d47c530a4173ecdb
Zaidis <chewybinx@gmail.com>
2022-04-03 01:37:13 -0500

    Menu Set up

---
+++ NaturalSelectionEditor/Assets/Scripts/Shop/ShopButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShopButton : MonoBehaviour
+{
+    public int m_cost;
+    public int id;
+    public Sprite m_Image;
+    public string m_title;
+    [TextArea()]
+    public string m_Description;
+
+    private void Start() {
+        
+    }
+
+
+
+}
